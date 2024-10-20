6ec00cb5d70e2ac1622efb38a0fa13833b321101
cosm1cgirl <166348211+cosm1cgirl@users.noreply.github.com>
2024-10-20 18:03:24 +0200

    Updated Footer.js
    Links to open on a new page.

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,28 @@ function Footer() {
   return (
     <footer className="footer">
       <p>
-        Coded By <a href="https://github.com/cosm1cgirl">Mary-Anne Schaffers</a>{" "}
+        Coded By{" "}
+        <a
+          href="https://github.com/cosm1cgirl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mary-Anne Schaffers
+        </a>{" "}
         | Hosted on{" "}
-        <a href="https://github.com/cosm1cgirl/weatherapp">GitHub</a> &{" "}
-        <a href="https://app.netlify.com/sites/weathernodejs/overview">
+        <a
+          href="https://github.com/cosm1cgirl/weatherapp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        &{" "}
+        <a
+          href="https://app.netlify.com/sites/weathernodejs/overview"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Netlify
         </a>
       </p>
